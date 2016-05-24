4be85f09d9c3b20145e0bd8cd6bace5602ca3284
Sean <sean.chen@ontario.ca>
2016-05-24 14:11:12 -0400

    create separate tasks for end to end testing on Windows.

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,30 @@ gulp.task('test:webdriver:jasmine', ['selenium'], function() {
     });
 });
 
+/**
+ * create separate task for testing on windows.
+ * Windows environment need some special steps...
+ */
+gulp.task('wintest:webdriver:jasmine', ['selenium-skip-install'], function() {
+    return gulp.src('test/wdio.conf.jasmine.js').
+    pipe(webdirverSingle({
+        //logLevel: 'verbose',
+        //logLevel: 'command',
+        logLevel: 'silent',
+        waitforTimeout: 12345,
+        // only for testing purposes
+        cucumberOpts: {
+            require: 'nothing'
+        },
+        reporter: 'spec'
+    })).once('end', function() {
+        // shutdown the selenium standalone server
+        selenium.child.kill();
+    });
+});
+
 var selenium = require('selenium-standalone');
+
 gulp.task('selenium', function (done) {
     selenium.install({
         logger: function (message) { 
@@ -51,5 +74,25 @@ gulp.task('selenium', function (done) {
           selenium.child = child;
           done();
         });
+    });
+});
+
+/**
+ * the task to start selenium directly.
+ * in some internal network behind firewall, it is hard to automatically
+ * donwload selenium and its drivers. 
+ * User need manually download everything and put to the proper folder.
+ * This task will load selenium once the manual steps are done.
+ */
+gulp.task('selenium-skip-install', function (done) {
+    selenium.start({
+       spawnOptions: {
+           stdio: 'inherit'
+       }
+    }, function (err, child) {
+      if (err) return done(err);
+
+      selenium.child = child;
+      done();
     });
 });
