5c62d05ec3abd396caea10dd7811938be860e866
Sean Chen <sean.chen@leocorn.com>
2017-06-02 10:34:25 -0400

    add the express to serve the static content

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,17 @@ gulp.task('hello', 'Hello Gulp World!', function() {
   console.log('Hello Gulp World!');
 });
 
+// using the express to serve static files.
+var gls = require('gulp-live-server');
+// the simplest express static server.
+//var liveServer = gls.static('.', 8900);
+// using a simple javascript file for express server.
+var liveServer = gls.new('test/express.js');
+gulp.task('express-app', function() {
+
+    liveServer.start();
+});
+
 // testing the webdriverio and selenium standalone
 var webdirverSingle = require('gulp-webdriver');
 
