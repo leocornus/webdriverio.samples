552a1d06872cdee3d59843db8d3a9f97085992d7
Sean Chen <sean.chen@leocorn.com>
2017-06-07 14:11:29 -0400

    add the test case to visit the same page many times, synchronously

--- test/wdio/simple-view-specs.js
+++ test/wdio/simple-view-specs.js
@@ -7,4 +7,34 @@ describe('specs to view a page', function() {
         // pause is in ms.
         browser.url(theUrl).pause(1000);
     });
+
+    it('Simply visit a page synchronously!', function(done) {
+
+        var theUrl = 'http://www.google.com';
+        var viewPage = function(howmany, i, callback) {
+
+            // pause is in ms.
+            browser.url(theUrl).pause(2000);
+            browser.call(callback);
+        };
+
+        // set how many time you ant to visit this page.
+        var howmany = 10;
+	if (howmany > 0) {
+            var loop = function(howmany, i, viewPage, loopDone) {
+                viewPage(howmany, i, function() {
+		    if(++i < howmany) {
+			setTimeout(function() {
+			    loop(howmany, i, viewPage, loopDone);
+			}, 0);
+	            } else {
+			browser.call(done);
+	            }
+	        });
+	    };
+	    loop(howmany, 0, viewPage, function(){});
+	} else {
+	    borwser.call(done);
+	}
+    });
 });
